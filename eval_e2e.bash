c15fbf1290a72fbfb0f7aa689fa2220a532184be
Yuchuan <yuchuan.7streams@gmail.com>
2025-10-28 18:16:07 +0800

    update for guanjie.
    Signed-off-by: Yuchuan <yuchuan.7streams@gmail.com>

--- eval_e2e.bash
+++ eval_e2e.bash
@@ -34,20 +34,12 @@ cd eval_tool_call
 mkdir output
 
 # Download the models
-hf download Qwen/Qwen3-8B 
-hf download meta-llama/Meta-Llama-3.1-8B-Instruct
 hf download openai/gpt-oss-20b
 
 # run the benchmark
-python3 bench_stagv2.py --dataset-path ./dataset/BFCL_v3_live_multiple.json --batch-size 1 128  --model-path Qwen/Qwen3-8B --grammar-backend xgrammar --output-path ./output/qwen-xgr.json 
-python3 bench_stagv2.py --dataset-path ./dataset/BFCL_v3_live_multiple.json --batch-size 1 128  --model-path Qwen/Qwen3-8B --grammar-backend llguidance --output-path ./output/qwen-llg.json 
-python3 bench_stagv2.py --dataset-path ./dataset/BFCL_v3_live_multiple.json --batch-size 1 128  --model-path Qwen/Qwen3-8B --grammar-backend outlines --output-path ./output/qwen-outlines.json 
-python3 bench_stagv2.py --dataset-path ./dataset/BFCL_v3_live_multiple.json --batch-size 1 128  --model-path meta-llama/Meta-Llama-3.1-8B-Instruct --grammar-backend xgrammar --output-path ./output/llama-xgr.json
-python3 bench_stagv2.py --dataset-path ./dataset/BFCL_v3_live_multiple.json --batch-size 1 128  --model-path meta-llama/Meta-Llama-3.1-8B-Instruct --grammar-backend llguidance --output-path ./output/llama-llg.json
-python3 bench_stagv2.py --dataset-path ./dataset/BFCL_v3_live_multiple.json --batch-size 1 128  --model-path meta-llama/Meta-Llama-3.1-8B-Instruct --grammar-backend outlines --output-path ./output/llama-outlines.json
-python3 bench_stagv2.py --dataset-path ./dataset/BFCL_v3_live_multiple.json --batch-size 1 128  --model-path openai/gpt-oss-20b --grammar-backend xgrammar --output-path ./output/oss-xgr.json
-python3 bench_stagv2.py --dataset-path ./dataset/BFCL_v3_live_multiple.json --batch-size 1 128  --model-path openai/gpt-oss-20b --grammar-backend llguidance --output-path ./output/oss-llg.json
-python3 bench_stagv2.py --dataset-path ./dataset/BFCL_v3_live_multiple.json --batch-size 1 128  --model-path openai/gpt-oss-20b --grammar-backend outlines --output-path ./output/oss-outlines.json
+python3 bench_stagv2.py --dataset-path ./dataset/BFCL_v3_live_multiple.json --batch-size 128  --model-path openai/gpt-oss-20b --grammar-backend xgrammar --output-path ./output/oss-xgr.json
+python3 bench_stagv2.py --dataset-path ./dataset/BFCL_v3_live_multiple.json --batch-size 128  --model-path openai/gpt-oss-20b --grammar-backend llguidance --output-path ./output/oss-llg.json
+python3 bench_stagv2.py --dataset-path ./dataset/BFCL_v3_live_multiple.json --batch-size 128  --model-path openai/gpt-oss-20b --grammar-backend outlines --output-path ./output/oss-outlines.json
 
 # generate the results
 cd ..
